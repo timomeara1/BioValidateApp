65f3aabadda424075ea005d7249851f5b14e94f6
Pat <patrick.carolan@gmail.com>
2012-02-11 10:38:34 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BioValidateApp::Application.config.secret_token = 'c5b2f6a8e7b2e145ce549970385bc1e3e1e47fd60f80456ad35ab019b785710dde924d9ee8cfc23c446c2df4f8e2759fbeb8faff6e24b5e26b9c1c8cfedee9f4'
